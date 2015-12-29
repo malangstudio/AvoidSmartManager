144bb52334eed10df81f417ab255a08ef008fc2f
Chiung Choi <god@malangstudio.com>
2015-12-29 21:31:57 +0900

    [수정] Service -> Activity 로 방식 변경 [제거] 필요없는 파일 제거

---
+++ app/src/main/java/com/malangstudio/avoidsmartmanager/AvoidSmartManagerActivity.java
@@ -0,0 +1,22 @@
+package com.malangstudio.avoidsmartmanager;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by god on 15. 12. 29..
+ */
+public class AvoidSmartManagerActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Toast.makeText(this, "AvoidSmartManagerActivity onCreate", Toast.LENGTH_LONG).show();
+
+        finish();
+
+        Log.d("chiung.choi", "AvoidSmartManagerActivity onCreate");
+    }
+}
--- app/src/main/java/com/malangstudio/avoidsmartmanager/AvoidSmartManagerReceiver.java
+++ app/src/main/java/com/malangstudio/avoidsmartmanager/AvoidSmartManagerReceiver.java
@@ -3,19 +3,18 @@ package com.malangstudio.avoidsmartmanager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.widget.Toast;
 
 /**
  * Created by Chiung Choi on 2015-12-20.
  */
-public class BootReceiver extends BroadcastReceiver {
+public class AvoidSmartManagerReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // 반복 코드 짜기 귀찮으니 부팅 시, 전원 연결/해제 시 처리
         if(intent.getAction() == Intent.ACTION_BOOT_COMPLETED || intent.getAction() == Intent.ACTION_POWER_CONNECTED || intent.getAction() == Intent.ACTION_POWER_DISCONNECTED) {
-            Intent serviceIntent = new Intent(context, AvoidSmartManagerService.class);
-            context.startService(serviceIntent);
+            Intent serviceIntent = new Intent(context, AvoidSmartManagerActivity.class);
+            serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(serviceIntent);
         }
     }
 }
