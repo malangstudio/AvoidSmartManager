e40b985e25906bd4a32b9a0f5f2a7359b54ee452
Chiung Choi <god@malangstudio.com>
2016-01-13 11:37:33 +0900

    [수정] 액티비티 실행 시 3초 내외의 랜덤값을 추가

--- app/src/main/java/com/malangstudio/avoidsmartmanager/AvoidSmartManagerReceiver.java
+++ app/src/main/java/com/malangstudio/avoidsmartmanager/AvoidSmartManagerReceiver.java
@@ -4,6 +4,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.Handler;
+
+import java.util.Random;
 
 /**
  * Created by Chiung Choi on 2015-12-20.
@@ -12,7 +15,7 @@ public class AvoidSmartManagerReceiver extends BroadcastReceiver {
     private final static String SMART_MANAGER_PACKAGE_NAME = "com.samsung.android.sm";
 
     @Override
-    public void onReceive(Context context, Intent intent) {
+    public void onReceive(final Context context, Intent intent) {
         if(Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())
                 || Intent.ACTION_POWER_CONNECTED.equals(intent.getAction())
                 || Intent.ACTION_POWER_DISCONNECTED.equals(intent.getAction())) {
@@ -25,9 +28,14 @@ public class AvoidSmartManagerReceiver extends BroadcastReceiver {
             }
 
             if(isSmartManagerExist) {
-                Intent serviceIntent = new Intent(context, AvoidSmartManagerActivity.class);
-                serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                context.startActivity(serviceIntent);
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent serviceIntent = new Intent(context, AvoidSmartManagerActivity.class);
+                        serviceIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        context.startActivity(serviceIntent);
+                    }
+                }, new Random().nextInt(3000));
             }
         }
     }
